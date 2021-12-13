509c861c77b07664f84dc0d215cf0c8e74d9a5bc
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-12-13 12:51:10 -0500

    pong window setup

---
+++ Python/Mini-Games/Pong/pong.py
@@ -0,0 +1,15 @@
+# Simple Pong Game
+
+import turtle
+
+win = turtle.Screen()
+win.title("Pong by Anthony Lofredo")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+
+# Main Game Loop
+while True:
+    win.update()
+
