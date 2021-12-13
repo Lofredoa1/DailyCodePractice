10862bbfdcc5cbbe88d9915e4b38506505bf49aa
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-12-13 13:57:09 -0500

    scorebboard added and speed increases

--- Python/Mini-Games/Pong/pong.py
+++ Python/Mini-Games/Pong/pong.py
@@ -1,12 +1,17 @@
 # Simple Pong Game
 
 import turtle
+import os
 
 win = turtle.Screen()
 win.title("Pong by Anthony Lofredo")
 win.bgcolor("black")
 win.setup(width=800, height=600)
 win.tracer(0)
+
+# Score 
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -35,6 +40,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.5
 ball.dy = 0.5
+
+# Scoreboard
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Functions
 def paddle_a_up():
@@ -76,24 +90,34 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system("afplay bounce.wav&")
         
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system("afplay bounce.wav&")
 
     if ball.xcor() > 390:
         ball.goto(0,0)
-        ball.dx *= -1
+        ball.dx = -0.5
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
     
     if ball.xcor() < -390:
         ball.goto(0,0)
-        ball.dx *= -1
+        ball.dx = 0.5
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and Ball Collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
-        ball.dx *= -1
+        ball.dx *= -1.2
+        os.system("afplay bounce.wav&")
         
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
-        ball.dx *= -1
+        ball.dx *= -1.2
+        os.system("afplay bounce.wav&")
