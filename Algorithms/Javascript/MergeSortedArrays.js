447b46224e624e4a627f0570ca1036e75e24f73a
Anthony Lofredo <anthony.lofredo@gmail.com>
2022-01-25 17:10:33 -0500

    merge sorted arrays solved

---
+++ Algorithms/Javascript/MergeSortedArrays.js
@@ -0,0 +1,24 @@
+//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+//The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+function mergeSortedArrays(nums1, m, nums2, n) {
+    let length = m + n;
+  
+    while (length > 0){
+        if(nums2[n-1] > nums1[m-1]){
+            nums1[length - 1] = nums2[n-1];
+            n--;
+            
+        } else {
+            nums1[length - 1] = nums1[m-1];
+            m--
+        }
+        length--
+    }
+    return nums1
+};
+
+console.log(mergeSortedArrays([1,2,3,0,0,0], 3, [2,5,6], 3))
