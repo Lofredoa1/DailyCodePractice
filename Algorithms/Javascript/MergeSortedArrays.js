816494d17dce5982b943a76be8173171bd489c50
Anthony Lofredo <anthony.lofredo@gmail.com>
2022-01-25 17:21:08 -0500

    merges sorted arrays bug fix

--- Algorithms/Javascript/MergeSortedArrays.js
+++ Algorithms/Javascript/MergeSortedArrays.js
@@ -5,20 +5,25 @@
 //The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 function mergeSortedArrays(nums1, m, nums2, n) {
-    let length = m + n;
+    let last = m + n - 1;
   
-    while (length > 0){
+    while (m > 0 && n > 0){
         if(nums2[n-1] > nums1[m-1]){
-            nums1[length - 1] = nums2[n-1];
+            nums1[last] = nums2[n-1];
             n--;
             
         } else {
-            nums1[length - 1] = nums1[m-1];
+            nums1[last] = nums1[m-1];
             m--
         }
-        length--
+        last--
+    }
+    while (n > 0){
+        nums1[last] = nums2[n-1];
+        n--;
+        last--
     }
     return nums1
 };
 
-console.log(mergeSortedArrays([1,2,3,0,0,0], 3, [2,5,6], 3))
+console.log(mergeSortedArrays([5,0], 1, [2], 1))
