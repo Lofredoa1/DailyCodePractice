7f1f2dc21ae93dc496341965fdb6d7f7b488343d
Anthony Lofredo <anthony.lofredo@gmail.com>
2022-04-07 19:40:51 -0400

    max subarray complete

--- Algorithms/Javascript/MaxSubarray.js
+++ Algorithms/Javascript/MaxSubarray.js
@@ -3,15 +3,12 @@
 //A subarray is a contiguous part of an array.
 
 function maxSubarray(nums){
-    let maxSum = nums[0];
-    let current = nums[0];
     if(nums.length === 0) return 0;
     
-    for (let i = 0; i < nums.length; i++){
-        current = Math.max(nums[i], current + nums[i]);
-        maxSum = Math.max(maxSum, current)
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
     }
-    return maxSum
+    return Math.max(...nums)
 };
 
 const input = [-2,1,-3,4,-1,2,1,-5,4];
